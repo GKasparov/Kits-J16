2f2ca45330a826c9c51e1c68335d299b4b02a7be
GKasparov <60962839+GKasparov@users.noreply.github.com>
2021-08-10 09:16:17 +1000

    Update KitsCommand.java

--- src/main/java/me/mazenz/kits/KitsCommand.java
+++ src/main/java/me/mazenz/kits/KitsCommand.java
@@ -5,7 +5,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
 import java.util.Objects;
 
 public class KitsCommand implements CommandExecutor {
