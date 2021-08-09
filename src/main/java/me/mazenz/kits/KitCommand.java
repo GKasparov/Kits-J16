bc2ca3610cca95a8ed29ac3510049f84a7b51ff4
GKasparov <60962839+GKasparov@users.noreply.github.com>
2021-08-10 09:15:39 +1000

    Update KitCommand.java

--- src/main/java/me/mazenz/kits/KitCommand.java
+++ src/main/java/me/mazenz/kits/KitCommand.java
@@ -8,9 +8,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
 import java.util.Objects;
-
 import static org.bukkit.Sound.*;
 
 public class KitCommand implements CommandExecutor {
