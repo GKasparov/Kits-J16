08641d6f4ea2859e727699bdd808344679374cb3
GKasparov <mizozahr@gmail.com>
2021-11-04 10:29:02 +1100

    Started VaultAPI hook

--- src/main/java/me/mazenz/kits/Kits.java
+++ src/main/java/me/mazenz/kits/Kits.java
@@ -1,5 +1,8 @@
 package me.mazenz.kits;
 
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -7,8 +10,12 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 public final class Kits extends JavaPlugin {
+
+    private static final Logger log = Logger.getLogger("Minecraft");
+    private static Economy econ = null;
 
     @Override
     public void onEnable() {
@@ -18,9 +25,30 @@ public final class Kits extends JavaPlugin {
             genPerms();
             Objects.requireNonNull(getCommand("kits")).setExecutor(new KitsCommand(this));
             Objects.requireNonNull(getCommand("kit")).setExecutor(new KitCommand(this));
+
+            if (getConfig().getBoolean("vaultEnabled")) {
+                if (!setupEconomy()) {
+                    log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+                    getServer().getPluginManager().disablePlugin(this);
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+
+        if (rsp == null) {
+            return false;
+        }
+
+        econ = rsp.getProvider();
+        return true;
     }
 
     @Override
